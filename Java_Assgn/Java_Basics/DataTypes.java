75798dac53473b5bf6518a6f530cd48f9c440e28
Ehthesham-Ahmed <117994804+Ehthesham-Ahmed@users.noreply.github.com>
2025-05-16 15:42:35 +0530

    data types prog

---
+++ Java_Assgn/Java_Basics/DataTypes.java
@@ -0,0 +1,27 @@
+// 4.  Define variables for different Data Types int, Boolean, char, float, double and print on the Console 
+
+package Java_Assgn.Java_Basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // syntax for integer: int <var> = <value>;
+        int a = 21;
+        // syntax for float: float <var> = <value>;
+        float b = 15.55f;
+        // syntax for double: double <var> = <value>;
+        double c = 1245.2568;
+        // syntax for char: char <var> = '<value>';
+        char d = 'H';
+        // syntax for String: String <var> = "<value>";
+        String s = "welcome";
+        // syntax for boolean: boolean <var> = true or false;
+        boolean i = true;
+
+        System.out.println("This is an integer: " + a);
+        System.out.println("This is a float: " + b);
+        System.out.println("This is a double: " + c);
+        System.out.println("This is a char: " + d);
+        System.out.println("This is a String: " + s);
+        System.out.println("This is a boolean: " + i);
+    }
+}

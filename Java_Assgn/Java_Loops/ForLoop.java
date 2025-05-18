af07674a13be49fb8c5fe3321e405a6ecb1a5f9d
Ehthesham-Ahmed <117994804+Ehthesham-Ahmed@users.noreply.github.com>
2025-05-18 10:11:42 +0530

    for loop

---
+++ Java_Assgn/Java_Loops/ForLoop.java
@@ -0,0 +1,12 @@
+// 1. Write a program to print  “Bright IT Career”  ten times using for loop
+package Java_Assgn.Java_Loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // for(initialization ; condition ; increment/decrement) { <code> }
+        System.out.println("Printing ten times using for loop: ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Bright IT Career");
+        }
+    }
+}
